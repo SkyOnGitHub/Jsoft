7c946cf26c1a154ccf02e3f00c1014bcb4b654af
SkylaOnGitHub <tobi@digitaldream.de>
2025-10-07 22:31:45 +0200

    added base classes for DTOs and converters

---
+++ Jsoft/Converters/TwitchDtoConverter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using JetBrains.Annotations;
+
+namespace Jsoft.Converters
+{
+    /// <summary>
+    /// Represents the abstract base class for a JSON converter for Twitch API data transfer objects.
+    /// </summary>
+    /// <typeparam name="TValue">The type of Twitch API data transfer object to convert to or from JSON.</typeparam>
+    [PublicAPI]
+    public abstract class TwitchDtoConverter< TValue > : JsonConverter<TValue>
+    where TValue : TwitchDto
+    {
+        /// <inheritdoc />
+        public override bool CanConvert( Type typeToConvert )
+            => canConvert(typeof(TValue), typeToConvert);
+        
+        private static bool canConvert( Type genericType, Type typeToConvert )
+            => genericType.IsAssignableFrom(typeToConvert);
+        
+        /// <summary>
+        /// Determines the type to convert to based on the specified <paramref name="reader"/> and supplied <paramref name="typeToConvert"/>.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
+        /// <param name="typeToConvert">The type to convert.</param>
+        /// <returns>The type determined to be converted.</returns>
+        protected abstract Type DetermineTypeToConvert( Utf8JsonReader reader, Type typeToConvert );
+        
+        private static void ensureCanConvert( Type genericType, Type typeToConvert )
+        {
+            if (!canConvert(genericType, typeToConvert)) throw new JsonException($"Type '{typeToConvert}' must be assignable to type '{genericType}'.");
+        }
+        
+        private static void ensureNotAbstract( Type typeToConvert )
+        {
+            if (typeToConvert.IsAbstract) throw new JsonException($"Type '{typeToConvert}' must be non-abstract.");
+        }
+        
+        private static void ensureNotSealed( Type genericType )
+        {
+            if (genericType.IsSealed) throw new JsonException($"Type must be '{genericType}'.");
+        }
+        
+        /// <inheritdoc />
+        [CanBeNull]
+        public override TValue Read( ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options )
+        {
+            var genericType = typeof(TValue);
+            
+            if (!genericType.Equals(typeToConvert)) {
+                ensureNotSealed(genericType);
+                ensureCanConvert(genericType, typeToConvert);
+            }
+            
+            if (!typeToConvert.IsSealed) {
+                typeToConvert = this.DetermineTypeToConvert(reader, typeToConvert);
+                ensureNotAbstract(typeToConvert);
+                ensureCanConvert(genericType, typeToConvert);
+            }
+            
+            return (TValue)JsonSerializer.Deserialize(ref reader, typeToConvert, options);
+        }
+        
+        /// <inheritdoc />
+        public override void Write( Utf8JsonWriter writer, TValue value, JsonSerializerOptions options )
+        {
+            var typeToConvert = value.GetType();
+            JsonSerializer.Serialize(writer, value, typeToConvert, options);
+        }
+    }
+}
---
+++ Jsoft/TwitchDto.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+using JetBrains.Annotations;
+
+namespace Jsoft
+{
+    /// <summary>
+    /// Represents the abstract base class for Twitch API data transfer objects.
+    /// </summary>
+    [PublicAPI]
+    public abstract class TwitchDto
+    {
+        /// <summary>
+        /// Parses the supplied JSON into a new instance of <see cref="TValue"/> using the specified <paramref name="options"/>.
+        /// </summary>
+        /// <param name="json">The JSON string to parse into a new instance of <see cref="TValue"/>.</param>
+        /// <param name="options">An object that specifies serialization options to use.</param>
+        /// <typeparam name="TValue">The type of Twitch API data transfer object to convert from JSON.</typeparam>
+        /// <returns>The new <see cref="TValue"/> instance.</returns>
+        /// <exception cref="JsonException">Unable to parse <typeparamref name="TValue"/> from JSON.</exception>
+        protected static TValue Parse< TValue >( string json, JsonSerializerOptions options )
+        where TValue : TwitchDto
+        {
+            var typeToConvert = typeof(TValue);
+            var deserialized  = JsonSerializer.Deserialize(json, typeToConvert, options) ?? throw new JsonException($"Unable to parse type '{typeToConvert}'.");
+            return (TValue)deserialized;
+        }
+        
+        /// <summary>
+        /// Parses the supplied JSON into a new instance of the <see cref="TwitchDto"/> class using the specified <paramref name="options"/>.
+        /// </summary>
+        /// <param name="json">The JSON string to parse into a new instance of <see cref="TwitchDto"/>.</param>
+        /// <param name="options">An object that specifies serialization options to use.</param>
+        /// <returns>The new <see cref="TwitchDto"/> instance.</returns>
+        public static TwitchDto Parse( string json, JsonSerializerOptions options = default )
+            => Parse<TwitchDto>(json, options);
+        
+        /// <summary>
+        /// Returns the JSON representation of the current instance using the specified <paramref name="options"/>.
+        /// </summary>
+        /// <param name="options">An object that specifies serialization options to use.</param>
+        /// <returns>The JSON representation of the current instance.</returns>
+        public string ToJson( JsonSerializerOptions options = default )
+        {
+            var typeToConvert = this.GetType();
+            return JsonSerializer.Serialize(this, typeToConvert, options);
+        }
+        
+        /// <summary>
+        /// Returns the JSON representation of the current instance.
+        /// </summary>
+        /// <returns>The JSON representation of the current instance.</returns>
+        /// <remarks>default(<see cref="JsonSerializerOptions"/>) is used for the serialization to JSON.</remarks>
+        public override string ToString()
+            => this.ToJson();
+    }
+}
